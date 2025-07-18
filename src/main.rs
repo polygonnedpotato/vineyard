7a20f82be3a02d2e3660b62b8806aadbb0416166
Arris Kathery <whotookelburg@hotmail.com>
2025-07-18 15:39:39 -0700

    screw daemons!

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,4 @@
-extern crate daemonize;
 
-use std::fs::File;
-
-use daemonize::Daemonize;
-
-fn main() {
-    let stdout = File::create("./test/vineyard.out").unwrap();
-    let stderr = File::create("./test/vineyard.err").unwrap();
-
-    let daemonize = Daemonize::new()
-        .pid_file("./test/vineyard.pid") // Every method except `new` and `start`
-        .chown_pid_file(true)      // is optional, see `Daemonize` documentation
-        .working_directory("./test/") // for default behaviour.
-        .user("nobody")
-        .group("daemon") // Group name
-        .group(2)        // or group id.
-        .umask(0o777)    // Set umask, `0o027` by default.
-        .stdout(stdout)  // Redirect stdout to `/tmp/daemon.out`.
-        .stderr(stderr)  // Redirect stderr to `/tmp/daemon.err`.
-        .privileged_action(|| "Executed before drop privileges");
-
-    match daemonize.start() {
-        Ok(_) => println!("Started the Vineyard daemon!"),
-        Err(e) => panic!("oh shit it failed!!: {}",e),
-    }
+fn main(){
+    print!("Vineyard {}",env!("CARGO_PKG_VERSION"));
 }
